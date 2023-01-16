06bb22926b46ca52bae2074df7baf17ff0d8b326
Hansen Andersen <hansen.sutikno@binus.ac.id>
2023-01-16 19:07:29 +0700

    slider and show hide
    still in progress

--- pages/accessability/bionic.jsx
+++ pages/accessability/bionic.jsx
@@ -2,102 +2,15 @@ import React, { useRef, useMemo, useState } from "react";
 import styled, { useTheme } from 'styled-components/native'
 import { View} from "react-native";
 import Text from "../../components/text";
-import BionicIcon from "../../components/icons/bionic-icon"
-import TextIcon from "../../components/icons/text-icon"
-import ThemeIcon from "../../components/icons/theme-icon"
+
 import HelpIcon from "../../components/icons/help-icon"
-
-
-const custom = {
-  BIONIC: 'bionic',
-  TEXT: 'text',
-  THEME: 'theme'
-}
 
 export default function BionicSection() {
   const theme = useTheme()
 
-  const snapPoints = useMemo(() => ["30%", "50%", "100%"], []);
-  const sheetRef = useRef(null);
-  const [selectedCustom, setSelectedCustom] = useState([])
-  
-  // custom top select
-  function customSelected(custom) {
-    return selectedCustom.includes(custom)
-  }
-  function toggleCustomFilter(custom) {
-    if (!customSelected(custom)) {
-      setSelectedCustom(selectedCustom => [
-        ...selectedCustom,
-        custom,
-      ])
-      return
-    }
-    setSelectedCustom(selectedCustom =>
-      selectedCustom.filter(
-        selectedCustom => selectedCustom !== custom
-      )
-    )
-  }
-
   return (
     <View>
-          <CustomContainer>
-            <ButtonContainer
-              onPress={() => toggleCustomFilter(custom.BIONIC)}
-              isSelected={customSelected(custom.BIONIC)}
-              >
-              <BionicIcon />
-              <Text
-                color={
-                  customSelected(custom.BIONIC)
-                  ? theme.colors.white
-                  : theme.colors.black
-                }
-                family={theme.typography.family.bold}
-                size={theme.typography.label.sz}
-                pl={5}
-                >
-                Bionic
-              </Text>
-            </ButtonContainer>
-            <ButtonContainer
-              onPress={() => toggleCustomFilter(custom.TEXT)}
-              isSelected={customSelected(custom.TEXT)}
-              >
-              <TextIcon />
-              <Text
-                color={
-                  customSelected(custom.TEXT)
-                  ? theme.colors.white
-                  : theme.colors.black
-                }
-                family={theme.typography.family.bold}
-                size={theme.typography.label.sz}
-                pl={5}
-                >
-                Text
-              </Text>
-            </ButtonContainer>
-            <ButtonContainer
-              onPress={() => toggleCustomFilter(custom.THEME)}
-              isSelected={customSelected(custom.THEME)}
-              >
-              <ThemeIcon />
-              <Text
-                color={
-                  customSelected(custom.THEME)
-                  ? theme.colors.white
-                  : theme.colors.black
-                }
-                family={theme.typography.family.bold}
-                size={theme.typography.label.sz}
-                pl={5}
-                >
-                Theme
-              </Text>
-            </ButtonContainer>
-          </CustomContainer>
+        <Container>
           <Text
             color={theme.colors.black}
             family={theme.typography.family.bold}
@@ -106,7 +19,9 @@ export default function BionicSection() {
             >
             Fixation
           </Text>
-          <HelpIcon />
+          <HelpIcon />5
+        </Container>
+        <Container>
           <Text
             color={theme.colors.black}
             family={theme.typography.family.bold}
@@ -116,10 +31,17 @@ export default function BionicSection() {
             Saccade
           </Text>
           <HelpIcon />
+        </Container>
     </View>
   );
 }
 
+const Container = styled.View`
+    display: flex;
+    justify-content:space-between;
+    flex-direction: row;
+    align-items: center;
+`
 const CustomContainer = styled.View`
   display: flex;
   justify-content: space-between;
@@ -141,5 +63,3 @@ const ButtonContainer = styled.Pressable`
     isSelected ? theme.colors.black : theme.colors.grey1};
   border-radius: 5px;
 `
-
-
--- pages/accessability/index.jsx
+++ pages/accessability/index.jsx
@@ -2,18 +2,57 @@ import React, { useCallback, useRef, useMemo, useState } from "react";
 import styled, { useTheme } from 'styled-components/native'
 import { StyleSheet,View, Button } from "react-native";
 import Text from "../../components/text";
-import BottomSheet, { BottomSheetView,BottomSheetSectionList,BottomSheetScrollView  } from "@gorhom/bottom-sheet";
+import BottomSheet, { BottomSheetView,BottomSheetSectionList } from "@gorhom/bottom-sheet";
 
 import ThemeSection from "./theme";
 import BionicSection from "./bionic";
 import TextSection from "./text";
 
+import BionicIcon from "../../components/icons/bionic-icon"
+import TextIcon from "../../components/icons/text-icon"
+import ThemeIcon from "../../components/icons/theme-icon"
+
+import BionicIconActive from "../../components/icons/bionic-icon-active"
+import TextIconActive from "../../components/icons/text-icon-active"
+import ThemeIconActive from "../../components/icons/theme-icon-active"
+
+import slider from "./slider";
+
+const custom = {
+  BIONIC: 'bionic',
+  TEXT: 'text',
+  THEME: 'theme'
+}
+
 export default function Button_Sheet() {
   const theme = useTheme()
 
+  const [selectedCustom, setSelectedCustom] = useState([])
+
+  const [shouldShow, setShouldShow] = useState(true);
+
+  // custom top select
+  function customSelected(custom) {
+    return selectedCustom.includes(custom)
+  }
+  function toggleCustomFilter(custom) {
+    if (!customSelected(custom)) {
+      setSelectedCustom(selectedCustom => [
+        ...selectedCustom,
+        custom,
+      ])
+      return
+    }
+    setSelectedCustom(selectedCustom =>
+      selectedCustom.filter(
+        selectedCustom => selectedCustom !== custom
+      )
+    )
+  }
+  
   const [index, setIndex] = useState(0);
   const [open, setOpen] = useState(false);
-  const snapPoints = useMemo(() => ["13%","50%"], []);
+  const snapPoints = useMemo(() => ["30%", "50%", "100%"], []);
   const sheetRef = useRef(null);
 
   const handleSheetChange = useCallback((index) => {
@@ -55,7 +94,13 @@ export default function Button_Sheet() {
     ),
     []
   );
+  // console.log(theme.typography.family.bold)
   return (
+    <View style={{flex: 1}}>
+      <Button title="Snap To 90%" onPress={() => handleSnapPress(2)} />
+      <Button title="Snap To 50%" onPress={() => handleSnapPress(1)} />
+      <Button title="Snap To 25%" onPress={() => handleSnapPress(0)} />
+      <Button title="Close" onPress={() => handleClosePress()} />
       <BottomSheet
         // style={{ position: 'absolute' }}
         ref={sheetRef}
@@ -63,13 +108,89 @@ export default function Button_Sheet() {
         initialSnapIndex={0}
         onChange={handleSheetChange}
       >
-        <BottomSheetScrollView>
+        <BottomSheetView>
         <Container>
+        <CustomContainer>
+            <ButtonContainer
+              onPress={() => toggleCustomFilter(custom.BIONIC) && setShouldShow(!shouldShow)}
+              isSelected={customSelected(custom.BIONIC)}
+              >
+               { <BionicIcon /> /*Here we will return the view when state is true 
+        and will return false if state is false*/}
+              {shouldShow ?
+              (
+                <BionicIconActive />
+              ) : null} 
+              <Text
+                color={
+                  customSelected(custom.BIONIC)
+                  ? theme.colors.white
+                  : theme.colors.black
+                }
+                family={theme.typography.family.bold}
+                size={theme.typography.label.sz}
+                pl={5}
+                >
+                Bionic
+              </Text>
+            </ButtonContainer>
+            <ButtonContainer
+              onPress={() => toggleCustomFilter(custom.TEXT)  && setShouldShow(!shouldShow)}
+              isSelected={customSelected(custom.TEXT)}
+              >
+              { <TextIcon /> /*Here we will return the view when state is true 
+        and will return false if state is false*/}
+              {shouldShow ?
+              (
+                <TextIconActive />
+              ) : null} 
+              
+              <Text
+                color={
+                  customSelected(custom.TEXT)
+                  ? theme.colors.white
+                  : theme.colors.black
+                }
+                family={theme.typography.family.bold}
+                size={theme.typography.label.sz}
+                pl={5}
+                >
+                Text
+              </Text>
+            </ButtonContainer>
+            <ButtonContainer
+              onPress={() => toggleCustomFilter(custom.THEME)  && setShouldShow(!shouldShow)}
+              isSelected={customSelected(custom.THEME)}
+              >
+              { <ThemeIcon /> /*Here we will return the view when state is true 
+        and will return false if state is false*/}
+              {shouldShow ?
+              (
+                <ThemeIconActive />
+              ) : null} 
+              
+              <Text
+                color={
+                  customSelected(custom.THEME)
+                  ? theme.colors.white
+                  : theme.colors.black
+                }
+                family={theme.typography.family.bold}
+                size={theme.typography.label.sz}
+                pl={5}
+                >
+                Theme
+              </Text>
+            </ButtonContainer>
+          </CustomContainer>
           < BionicSection />
           < TextSection />
           < ThemeSection />
+
+          {/* <Text>Awesome 🔥</Text> */}
+
         </Container>
-        </BottomSheetScrollView>
+        </BottomSheetView>
         {/* <BottomSheetView>
           <Text>Awesome 🔥</Text>
           </BottomSheetView>
@@ -84,6 +205,7 @@ export default function Button_Sheet() {
           contentContainerStyle={styles.contentContainer}
         /> */}
       </BottomSheet>
+    </View>
   );
 }
 // const styles = StyleSheet.create({
@@ -106,6 +228,27 @@ export default function Button_Sheet() {
 // });
 const Container = styled.View`
   margin: 20px;
+`
+const CustomContainer = styled.View`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+`
+const ButtonContainer = styled.Pressable`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 8px 10px;
+  border: 2px solid #E0E0E0;
+  background-color: ${({ theme, isSelected }) =>
+    isSelected ? theme.colors.black : theme.colors.white};
+  border-color: ${({ theme, isSelected }) =>
+    isSelected ? theme.colors.black : theme.colors.grey1};
+  border-radius: 5px;
 `
 
 
