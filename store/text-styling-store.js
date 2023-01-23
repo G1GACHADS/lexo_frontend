f774c980da2d6c30eff98e3fd1e3dcf44ca05584
HotPotatoC <juandotulung@gmail.com>
2023-01-23 19:11:25 +0700

    feat(store): added fontFamily state

--- store/text-styling-store.js
+++ store/text-styling-store.js
@@ -1,9 +1,15 @@
 import { create } from 'zustand'
+import theme from '../theme'
 
 export const useTextStyleStore = create((set) => ({
+  fontFamily: theme.typography.family.medium,
   size: 14,
   lineHeight: 25,
   letterSpacing: 0.05,
+  changeFontFamily: (family) =>
+    set((state) => ({
+      fontFamily: family,
+    })),
   changeTextSize: (size) =>
     set((state) => ({
       size: size,
