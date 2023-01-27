fc3b20b3c86f338070c03617b44669e486cb93ee
HotPotatoC <juandotulung@gmail.com>
2023-01-28 00:22:22 +0700

    chore: debug

--- pages/result/index.jsx
+++ pages/result/index.jsx
@@ -1,6 +1,6 @@
 import { useFocusEffect } from '@react-navigation/native'
 import { useCallback } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native'
 import styled from 'styled-components'
 import { useReadingThemeStore } from '../../store/reading-theme-store'
 import { toggleSettings } from '../../store/toggle-settings-store'
